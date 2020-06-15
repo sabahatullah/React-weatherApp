7a9272533393234d355fd5731feae3e4176d3e9d
HappyDev <sabahat.ullah@aol.com>
2020-06-15 13:33:38 +0100

    Add files via upload

---
+++ build/precache-manifest.731f18471127afefbd0a3409ee4b8363.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa8b86fe8d012915892a4a246aac44d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afb9dfb2308742b061a6",
+    "url": "/static/css/main.9acbb4b5.chunk.css"
+  },
+  {
+    "revision": "6489a7e88d14e7475029",
+    "url": "/static/js/2.c2734d3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2734d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afb9dfb2308742b061a6",
+    "url": "/static/js/main.6ac2d2df.chunk.js"
+  },
+  {
+    "revision": "9f1e78d60d8b52bd3d90",
+    "url": "/static/js/runtime-main.a86148e1.js"
+  },
+  {
+    "revision": "18350a8959bd593a073364fecd13e482",
+    "url": "/static/media/clear-img.18350a89.jpg"
+  },
+  {
+    "revision": "15d8aff8c37395f97fadcc11c3f22484",
+    "url": "/static/media/clouds-img.15d8aff8.jpg"
+  },
+  {
+    "revision": "540c5135553557fd7ab58f6fb3ebcdf0",
+    "url": "/static/media/smoke-img.540c5135.jpg"
+  }
+]);
